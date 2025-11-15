4ef0f430bc3ff3527d6d56848c627d4eb8e17133
vantasyman <vantasyman@users.noreply.github.com>
2025-11-15 16:06:11 +0000

    deploy: d32d9cee94912c3eb75f1a07d80eaee4f2f00532

--- js/supabase_client.js
+++ js/supabase_client.js
@@ -5,8 +5,8 @@
 const { createClient } = window.supabase;
 
 // ⬇️ 这些是将被 GitHub Action 自动替换的占位符
-const SUPABASE_URL = '%SUPABASE_URL%';
-const SUPABASE_ANON_KEY = '%SUPABASE_ANON_KEY%';
+const SUPABASE_URL = 'https://cmgphbvuoebxfufjzwpa.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtZ3BoYnZ1b2VieGZ1Zmp6d3BhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxNzA5NjIsImV4cCI6MjA3ODc0Njk2Mn0.nQvnDtbublyN-V9RmIoxP5L-PVRkC9inNtAWyONV-Nk';
 
 if (SUPABASE_URL.includes('%')) {
   console.error(
